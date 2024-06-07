3b306c7f97eeddfe138fbdf84db3b7d58d437e40
Israel Batista <israel.silvino@gmail.com>
2024-06-07 16:44:36 -0300

    refactor: Make all optional fields on Token use undefined for consistency

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,8 +1,8 @@
 export type Token = {
     // Parent is the key of the parent object, or undefined if it is the root object
     parent?: string,
-    // Name is the key of the current object, or null if it is an array element
-    name: string | null,
+    // Name is the key of the current object, or undefined if it is an array element
+    name?: string,
     _type: TokenType,
     value: string | number | boolean | Object | any[] | null,
 }
